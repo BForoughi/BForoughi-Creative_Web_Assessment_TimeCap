b6bb14c25d962614d23ee2591a48935a2ef03c5f
BForoughi <147166034+BForoughi@users.noreply.github.com>
2025-11-11 12:04:18 +0000

    checking users sessions and adding error handling for invalid sessions

--- submission-point-2/simpleSocialStarter/index.js
+++ submission-point-2/simpleSocialStarter/index.js
@@ -1,4 +1,4 @@
-// requiring node modules
+// -------requiring node modules-------
 const dotenv = require("dotenv");
 dotenv.config();
 const express= require('express');
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3000
 const sessions = require('express-session');
 const cookieParser = require('cookie-parser')
 
-// requiring the module exports
+// -------requiring the module exports-------
 const posts = require('./models/posts');
 const users = require('./models/users');
 
@@ -21,6 +21,7 @@ app.listen(PORT, ()=>{
     console.log(`Server running at http://localhost:${PORT}`)
 })
 
+// -------initialising node modules-------
 app.use(express.static('public'))
 app.use(express.urlencoded({extended: false}))
 
@@ -31,14 +32,29 @@ app.use(sessions({
     saveUninitialized: false
 }))
 
+// -------APP CONTROLLERS-------
 app.get('/app', (request, response)=>{
-    response.sendFile(path.join(__dirname, '/views', 'app.html'))
+    // checks for a session
+    if(request.session){
+        // checks for a valid session with a valid username
+        if(request.session.username){
+            response.sendFile(path.join(__dirname, '/views', 'app.html'))
+        } else{
+            // if not a valid username destroy session and redirect to not logged in
+            request.session.destroy()
+            response.sendFile(path.join(__dirname, '/views', 'notloggedin.html'))
+        }
+    } else{
+        // if there is no session redirect to not logged in 
+        response.sendFile(path.join(__dirname, '/views', 'notloggedin.html'))
+    }
+    
 })
 
 // controller that passes the inputted message from the front into the newPost function in the backend (posts.js)
 app.post('/newpost', (request, response)=>{
     //console.log(request.body)
-    posts.newPost(request.body.message, "userX")
+    posts.newPost(request.body.message, request.body.username)
     response.sendFile(path.join(__dirname, '/views', 'app.html'))
 })
 
@@ -58,6 +74,7 @@ app.get('/login', (request, response)=>{
 // login controller
 app.post('/login', (req, res)=>{
     if(users.checkUser(req.body.username, req.body.password)){
+        req.session.username=req.body.username
         res.sendFile(path.join(__dirname, '/views', 'app.html'))
     } else{
         //else false log invalid login and send to login failed
