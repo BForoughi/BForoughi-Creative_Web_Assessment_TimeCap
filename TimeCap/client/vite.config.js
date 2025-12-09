032951c7e0718578df3641832dd220db775eb7da
BForoughi <147166034+BForoughi@users.noreply.github.com>
2025-12-09 11:32:39 +0000

    setting up my express server

--- TimeCap/client/vite.config.js
+++ TimeCap/client/vite.config.js
@@ -3,4 +3,14 @@ import react from "@vitejs/plugin-react"
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+    // This will redirect any requests to yourwebsite.com/api to the Express.js server
+      "/api": {
+      // The port you use here should match the "PORT" variable in your Express.js server
+      target: "http://localhost:3000",
+      changeOrigin: true,
+      },
+    },
+  },
 })
---
+++ TimeCap/server/main.js
@@ -0,0 +1,26 @@
+// -------Requiring node modules-------
+const dotenv = require("dotenv");
+dotenv.config();
+const express = require('express');
+const app = express();
+const path = require('path');
+const sessions = require('express-session');
+const cookieParser = require('cookie-parser');
+const mongoose=require('mongoose');
+const MongoStore = require("connect-mongo");
+
+// -------MongoDB-------
+const mongoPassword = process.env.MONGODB_PASSWORD
+const mongoUsername = process.env.MONGODB_USERNAME
+const mongoAppName = process.env.MONGODB_MYAPPNAME
+
+const connectionString = `mongodb+srv://${mongoUsername}:${mongoPassword}@timecap.jjo4ept.mongodb.net/${mongoAppName}?retryWrites=true&w=majority`
+mongoose.connect(connectionString)
+
+// ----------server controller---------
+const PORT = process.env.PORT || 4000
+app.listen(PORT, ()=>{
+    console.log(`Server running at http://localhost:${PORT}`)
+})
+
+app.use(express.json())
